031822140bf52f7bd30e8b08d3b6e7ecf72f4a2c
bumpjammy <bumpjammy@gmail.com>
2023-08-26 20:21:32 +0100

    Initialise Vectors

--- src/app_loop.rs
+++ src/app_loop.rs
@@ -1,14 +1,14 @@
 use sdl2::EventPump;
+use sdl2::rect::Point;
 use sdl2::render::WindowCanvas;
 use sdl2::sys::SDL_Renderer;
-use crate::{event_handling, rendering};
+use crate::{CIRCLE_CENTRE, event_handling, rendering};
 
 pub(crate) fn run_app_loop(canvas: &mut WindowCanvas, event_pump: &mut EventPump) {
     let mut frame = 0;
-    let mut sine_points = Vec::new();
-    let mut cosine_points = Vec::new();
-    let mut tangent_points = Vec::new();
-
+    let mut sine_points = Vec::with_capacity(1001);
+    let mut cosine_points = Vec::with_capacity(1001);
+    let mut tangent_points = Vec::with_capacity(1001);
     'running: loop {
         rendering::render(canvas, frame, &mut sine_points, &mut cosine_points, &mut tangent_points);
 
